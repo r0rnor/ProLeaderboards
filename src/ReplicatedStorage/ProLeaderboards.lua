cd089824e13a5fe0b4d22d064f5eef9e787c9098
r0rnor <ilia.papirus@gmail.com>
2024-03-17 22:11:41 +0200

    Tested, added several things

--- src/ReplicatedStorage/ProLeaderboards.lua
+++ src/ReplicatedStorage/ProLeaderboards.lua
@@ -1,11 +1,19 @@
+-- stylua: ignore start
 local DataStoreService = game:GetService("DataStoreService")
+local RunService = game:GetService("RunService")
 local Signal = require(game.ReplicatedStorage.Packages.Signal)
+
+
 
 export type PageSettings = {
 	Ascending: boolean,
 	PageSize: number,
 	MinValue: number,
 	MaxValue: number,
+}
+
+export type UISettings = {
+	List : ScrollingFrame
 }
 
 export type Leaderboard = {
@@ -17,59 +25,80 @@ export type Leaderboard = {
 	UpdateUI: RBXScriptSignal,
 }
 
+
+
 local ProLeaderboards = {}
 ProLeaderboards.__index = ProLeaderboards
 
-function ProLeaderboards.new(DataStoreName: string?, UpdateTime: number?, Ascending: boolean?, PageSize: number?, MinValue: number?, MaxValue: number?)
+function ProLeaderboards.new(DataStoreName: string?, UpdateTime: number?, PageSettings : PageSettings?)
 	local self: Leaderboard = setmetatable({}, ProLeaderboards)
 
 	self.DataStoreName = DataStoreName or "Basic"
 	self.DataStore = DataStoreService:GetOrderedDataStore(self.DataStoreName)
 	self.UpdateTime = UpdateTime or 60
 	self.UpdateUI = Signal.new()
-	self.PageSettings = {
-		Ascending = Ascending or true,
-		PageSize = PageSize and math.clamp(PageSize, 1, 100) or 100,
-		MinValue = MinValue,
-		MaxValue = MaxValue,
-	}
 
-	while true do
-		task.wait(20)
-		self.UpdateUI:Fire(ProLeaderboards.GetPages(self)[1])
-	end
+	self.PageSettings = PageSettings or {Ascending = false, PageSize = 100}
+	self.PageSettings.Ascending = self.PageSettings.Ascending or false
+	self.PageSettings.PageSize = self.PageSettings.PageSize and math.clamp(self.PageSettings.PageSize, 0, 100) or 100
+
+	local CurrentTime = 0
+	RunService.Heartbeat:Connect(function(deltaTime)
+		CurrentTime += deltaTime
+		if CurrentTime < self.UpdateTime then return end
+		CurrentTime = 0
+		self.UpdateUI:Fire(self:GetPages(1)[1])
+	end)
 
 	return self
 end
 
-function ProLeaderboards.GetPages(self: Leaderboard)
-	local ResultPages = {}
+function ProLeaderboards:GetPages(NumberOfPages : number?)
+	local self: Leaderboard = self
+	local NumberOfPages = NumberOfPages or 10^4
 
-	local Pages = self.DataStore:GetSortedAsync(self.PageSettings.Ascending, self.PageSettings.PageSize, self.PageSettings.MinValue, self.PageSettings.MaxValue)
+	local Pages : DataStorePages = self.DataStore:GetSortedAsync(self.PageSettings.Ascending, self.PageSettings.PageSize, self.PageSettings.MinValue, self.PageSettings.MaxValue)
+
+	local ResultPages = {}
+	local PageIndex : number = 1
+	local Rank : number = 0
 
 	repeat
 		local Entries = Pages:GetCurrentPage()
+		ResultPages[PageIndex] = {}
 
-		table.insert(ResultPages, Entries)
-
-		if not Pages.IsFinished then
-			Pages:AdvanceToNextPageAsync()
+		for _, Entry in pairs(Entries) do
+			Rank += 1
+			ResultPages[PageIndex][Rank] = Entry
 		end
-	until Pages.IsFinished
 
-	print(ResultPages)
+		if not Pages.IsFinished then Pages:AdvanceToNextPageAsync() PageIndex += 1 end
+	until Pages.IsFinished or PageIndex > NumberOfPages
 
 	return ResultPages
 end
 
-function ProLeaderboards.SetValue(self: Leaderboard, Player: Player, NewValue: any)
-	self.DataStore:SetAsync(Player.UserId, NewValue)
+function ProLeaderboards:SetValue(Player: Player | string, NewValue: any)
+	local self: Leaderboard = self
+
+	assert(Player, "Player or string wasn't provided to :SetValue")
+	assert(NewValue, "New value wasn't provided to :SetValue")
+
+	local SetKey = typeof(Player) ~= "string" and Player.UserId or Player
+
+	self.DataStore:SetAsync(SetKey, NewValue)
 end
 
-function ProLeaderboards.ConnectValueInstance(self: Leaderboard, Player: Player, ValueInstance: ValueBase)
+function ProLeaderboards:ConnectValue(Player: Player | string, ValueInstance: ValueBase)
+	local self: Leaderboard = self
+
+	self:SetValue(Player, ValueInstance.Value)
+
 	ValueInstance.Changed:Connect(function()
-		ProLeaderboards.SetValue(self, Player, ValueInstance.Value)
+		self:SetValue(Player, ValueInstance.Value)
 	end)
 end
 
 return ProLeaderboards
+
+-- stylua: ignore end
--- src/ServerScriptService/Test.server.lua
+++ src/ServerScriptService/Test.server.lua
@@ -1,4 +1,12 @@
-local ProLeaderboards = require(game.ReplicatedStorage.ProLeaderboards)
+local ProLeaderboards = require(game.ReplicatedStorage.ProLeaderboards.ProLeaderboards)
 
-local Leaderboard = ProLeaderboards.new()
-print(Leaderboard)
+local Leaderboard = ProLeaderboards.new("CoinsStore", 5, { PageSize = 5 })
+Leaderboard:ConnectValue("TestBeast", workspace.TestBeastCoins)
+Leaderboard:SetValue("MrBeast2", 30)
+Leaderboard:SetValue("MrBeast3", 420)
+Leaderboard:SetValue("MrBeast4", 10)
+Leaderboard:SetValue("MrBeast5", 1488)
+
+Leaderboard.UpdateUI:Connect(function(Pages: {})
+	print("Here is the 1st page!: ", Pages)
+end)
